311d4ec1e8b679ad49dee012f3cce574aae00bc8
NtyamK <ntyam.kevin@univ-grenoble-alpes.fr>
2025-02-05 16:02:07 +0100

    Ajout de la documentation Doxygen. (Executer index.html pour le site)

--- 2Semaine-27jan/include/hash.h
+++ 2Semaine-27jan/include/hash.h
@@ -6,6 +6,14 @@
 
 #define MaxSizeArray 16381
 
+/**
+ * @brief Calcule le hash d'un mot.
+ * 
+ * Cette fonction génère un code hash à partir d'une chaîne de caractères.
+ * 
+ * @param m Le mot.
+ * @return La valeur.
+ */
 int hash(char *m);
 
 #endif
--- 2Semaine-27jan/include/word.h
+++ 2Semaine-27jan/include/word.h
@@ -29,9 +29,49 @@ struct mot_t {
   mot_data_t data;
 };
 
+/**
+ * @brief Récupère le prochain mot dans un fichier.
+ * 
+ * Cette fonction lit un fichier et extrait le prochain mot, en mettant à jour
+ * les lignes et colonnes.
+ * 
+ * @param f Fichier à lire.
+ * @param nblin Pointeur vers le nombre de lignes.
+ * @param nbcol Pointeur vers le nombre de colonnes.
+ * @return Le mot extrait.
+ */
 char *next_word(FILE *f, unsigned int *nblin, unsigned int *nbcol);
+
+/**
+ * @brief Incrémente la position d'un mot dans la liste des emplacements.
+ * 
+ * Cette fonction enregistre une nouvelle occurrence d'un mot avec sa position.
+ * 
+ * @param location Pointeur vers l'emplacement du mot.
+ * @param line Numéro de la ligne.
+ * @param colonne Numéro de la colonne.
+ */
 void incWord(emplacement_t* location, unsigned int line, unsigned int colonne);
+
+/**
+ * @brief Affiche un mot dans un fichier.
+ * 
+ * Cette fonction affiche un mot et ses emplacements dans un fichier donné.
+ * 
+ * @param word Le mot à afficher.
+ * @param filedes Fichir où afficher le mot.
+ */
 void displayWord(mot_data_t* word, FILE *filedes);
+
+/**
+ * @brief Compare deux mots.
+ * 
+ * Une fonction compare les données de deux mots.
+ * 
+ * @param w1 Premier mot.
+ * @param w2 Deuxième mot.
+ * @return 0 si les mots sont égaux, une valeur différente sinon.
+ */
 int compareWord(mot_data_t* w1, mot_data_t* w2);
 
 #endif
